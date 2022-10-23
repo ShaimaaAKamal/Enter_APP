4bbc5f56f635cc3261589dbdad805613fb31d926
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-10-23 15:34:01 +0200

    order modules

---
+++ js/login.js
@@ -0,0 +1,46 @@
+import { Validation } from "./modules/Validation/validate.js";
+import { ClearInputs } from "./modules/Validation/clearModule.js";
+
+const validation=new Validation();
+const clearInputs=new ClearInputs();
+const loginBtn=document.querySelector('#loginBtn');
+const userEmail=document.querySelector('#userEmail');
+const userPassword=document.querySelector('#userPassword');
+const existed=document.getElementById('existed');
+const users=(localStorage.getItem('users'))?JSON.parse(localStorage.getItem('users')):[];
+
+
+
+function handleLogin(){
+    if(!existed.classList.contains('d-none'))  existed.classList.add('d-none');
+      const password=userPassword.value;
+      const email=userEmail.value;
+      if(validation.formValidate(userEmail,userPassword,clearInputs.clearValidInputs,clearInputs.createInvalidMessage)){
+        const existedUser= users.find(user=>user.email === email);
+        if(!existedUser){
+            clearInputs.createInvalidMessage(email,userEmail,`This Email isn't exist`);
+         }
+         else{
+             if(existedUser.password !== password) existed.classList.remove('d-none');
+             else{
+                 console.log('logged')
+             }
+         }
+        
+      }
+}
+
+
+loginBtn.addEventListener('click',handleLogin);
+
+userEmail.addEventListener('focus',(e)=>{
+    if(!existed.classList.contains('d-none'))  existed.classList.add('d-none');
+    clearInputs.clearValidInputs(e.target);
+})
+
+
+userPassword.addEventListener('focus',(e)=>{
+    if(!existed.classList.contains('d-none'))  existed.classList.add('d-none');
+    clearInputs.clearValidInputs(e.target);
+})
+
