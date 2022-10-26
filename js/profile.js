6f6f531af7f4a424b3a87aaadce60eab6bb4b6ca
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-10-26 13:39:25 +0200

    start change password feature

--- js/profile.js
+++ js/profile.js
@@ -1,19 +1,27 @@
-import {Validation} from './modules/Validation/validate.js' 
-import {ClearInputs} from './modules/Validation/clearModule.js'
+import {Validation} from './modules/Validation/validate.js';
+import {ClearInputs} from './modules/Validation/clearModule.js';
+
+const validate=new Validation();
+const clearInputs=new ClearInputs();
+const user=JSON.parse(localStorage.getItem('user'));
+const users=JSON.parse(localStorage.getItem('users'));
 
 export function displayProfile(){
-    const validate=new Validation();
-    const clearInputs=new ClearInputs();
+
     const userName=document.querySelector('#userName');
     const userEmail=document.querySelector('#userEmail');
     const userType=document.getElementById('userType')
-    const user=JSON.parse(localStorage.getItem('user'));
-    const users=JSON.parse(localStorage.getItem('users'));
+   
     const changeEmailBlock=document.querySelector('#changeEmail');
     const changeEmailLink=document.querySelector('#changeEmailLink');
-    const userChangeEmailInput=document.getElementById('userChangeEmail');
-    const closeIcon=document.querySelector('.closeIcon');
+    const changePasswordLink=document.querySelector('#changePasswordLink');
+    const changePasswordBlock=document.querySelector('#changePassword');
+   
+    const closeEmailIcon=document.querySelector('.closeEmailIcon');
+    const closePasswordIcon=document.querySelector('.closePasswordIcon');
     const changeEmailBtn=document.querySelector('#changeEmailBtn');
+    const changePasswordBtn=document.querySelector('#changePasswordBtn');
+
     userName.innerHTML=(user.name)?user.name:'UserName';
     userEmail.innerHTML=(user.email)?user.email:'example@info.com';
     userType.innerHTML=(user.type)?user.type:'Regular';
@@ -22,13 +30,35 @@ export function displayProfile(){
     changeEmailLink.addEventListener('click',(e)=>{
         e.preventDefault();
         changeEmailBlock.classList.remove('d-none');
-    })
+    });
 
-    closeIcon.addEventListener('click',()=>{
+    changePasswordLink.addEventListener('click',(e)=>{
+        e.preventDefault();
+        changePasswordBlock.classList.remove('d-none');
+    });
+
+    closeEmailIcon.addEventListener('click',()=>{
         changeEmailBlock.classList.add('d-none');
-    })
-    changeEmailBtn.addEventListener('click',(e)=>{
-         const newEmail=userChangeEmailInput.value
+    });
+
+    closePasswordIcon.addEventListener('click',()=>{
+        changePasswordBlock.classList.add('d-none');
+    });
+
+    changeEmailBtn.addEventListener('click',()=>{
+        changeEmail(changeEmailBlock);
+    });
+
+    changePasswordBtn.addEventListener('click',()=>{
+        changePassword(changePasswordBlock);
+    });
+
+}
+
+
+function changeEmail(changeEmailBlock){
+   const userChangeEmailInput=document.getElementById('userChangeEmail');
+   const newEmail=userChangeEmailInput.value
          if(validate.validateEmail(newEmail)){
             clearInputs.clearValidInputs(userChangeEmailInput);
             user.email=newEmail;
@@ -40,8 +70,11 @@ export function displayProfile(){
          }
          else{
             clearInputs.createInvalidMessage(newEmail,userChangeEmailInput,'E-mail is invalid') 
-
          }
-    })
 }
 
+function changePassword(changePasswordBlock){
+    const oldPassword=document.getElementById('oldPassword').value;
+    const newPassword=document.getElementById('newPassword').value;
+    const newConfirmPassword=document.getElementById('newConfirmPassword').value;
+}
